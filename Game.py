d06e0ef4c5ea7eba88390c05bf56941def10a2a3
Victor Samsonov <64097523+victorsamsonov@users.noreply.github.com>
2021-01-18 13:15:29 -0600

    Update Game.py

--- Game.py
+++ Game.py
@@ -5,7 +5,6 @@ from Board import Board
 
 
 class Game(Board):
-
     screen = pygame.display.set_mode((720, 720))
     pygame.display.set_caption("Victor Samsonov Sudoku")
     # Allows to deselect the boxes
@@ -22,7 +21,7 @@ class Game(Board):
         self.difficulty_state = False
         # Start section state
         self.start_state = False
-        self.w, self.h = 720, 720
+        self.w, self.h = 1080, 1000
         self.display = pygame.Surface((self.w, self.h))
         self.window = pygame.display.set_mode((self.w, self.h))
         self.font = '8-BIT WONDER.TTF'
@@ -46,29 +45,38 @@ class Game(Board):
         # avoid multiple menu selections in a row
         self.transition = False
         self.empty = None
+        self.menu_bg_color = (176,190,197 ,1)
 
     # Renders the window where the game takes place
     def redraw_window(self, time):
         lm = 70
-        self.window.fill((189, 189, 189, 1))
+
+        self.window.fill(self.menu_bg_color)
+        pygame.draw.circle(self.window, (0, 0, 0, 1), (1015, 50), 30)
+        pygame.draw.rect(self.window, (0, 0, 0, 1), pygame.Rect(985, 50, 60, self.chances * 67.5))
+        pygame.draw.circle(self.window, (0, 0, 0, 1), (1015, int(self.chances * 67.5 + 50)), 30)
         fnt = pygame.font.SysFont("comicsans", 55)
         fnt2 = pygame.font.SysFont('comicsans', 70)
         time = fnt.render("Time " + self.format_time(time), 1, (76, 175, 80, 1))
-        menu = fnt.render("M = Back to Menu", 1, (0, 0, 0, 1))
+        menu = fnt.render("M = Back to Menu", 1, (255, 255, 255, 1))
         complete = fnt.render("Press C to complete the board", 1, (0, 0, 0, 1))
-        self.window.blit(time, (15, 650))
-        self.window.blit(menu, (self.w // 2, 650))
-        self.window.blit(complete, (75, 20))
+        pygame.draw.circle(self.window, (0, 0, 0, 1), (110, 920), 80)
+        pygame.draw.circle(self.window, (0, 0, 0, 1), (int(self.w // 1.11), 920), 80)
+        pygame.draw.rect(self.window, (0, 0, 0, 1), pygame.Rect(100, 840, 880, 160))
+
+        self.window.blit(time, (105, 900))
+        self.window.blit(menu, (self.w // 1.65, 900))
+        self.window.blit(complete, (260, 120))
         error = fnt2.render("X ", 2, (255, 0, 0))
         chance = fnt2.render("X ", 2, (76, 175, 80, 1))
         position = []
         position2 = []
         # Updates the chances and strikes
         for i in range(self.chances):
-            position2.append((640, 40 + (i * 50) + lm))
+            position2.append((1000, 40 + (i * 50) + lm))
             self.window.blit(chance, position2[i])
         for i in range(self.strikes):
-            position.append((640, 40 + (i * 50) + lm))
+            position.append((1000, 40 + (i * 50) + lm))
             self.window.blit(error, position[i])
         self.board.render(self.window)
 
@@ -76,7 +84,7 @@ class Game(Board):
     def render_menu(self):
         # self.w // 2 x centered
         y_const = 85
-        self.window.fill((189, 189, 189, 1))
+        self.window.fill(self.menu_bg_color)
         fnt = pygame.font.SysFont("8-BIT-WONDER", 85)
         fnt2 = pygame.font.SysFont("8-BIT-WONDER", 35)
         fn3 = pygame.font.SysFont("8-BIT-WONDER", 105)
@@ -92,12 +100,12 @@ class Game(Board):
         self.window.blit(Difficulty, (self.w // 2 - 130, self.h // 2 + 2 * y_const - 130))
         self.window.blit(Quit, (self.w // 2 - 67, self.h // 2 + 3 * y_const - 130))
         self.window.blit(self.cursor_rect, (self.cursor_x, self.cursor_y))
-        self.window.blit(Info, (20, self.h / 1.1))
+        self.window.blit(Info, (180, self.h / 1.1))
 
     # renders difficulty menu
     def render_difficulty(self):
         y_const = 85
-        self.window.fill((189, 189, 189, 1))
+        self.window.fill(self.menu_bg_color)
         fnt = pygame.font.SysFont("8-BIT-WONDER", 85)
         Difficulty = fnt.render("Difficulty Menu ", 1, (255, 255, 255, 255))
         if self.chances == 10:
@@ -123,7 +131,7 @@ class Game(Board):
     # renders start
     def render_start(self):
         y_const = 85
-        self.window.fill((189, 189, 189, 1))
+        self.window.fill(self.menu_bg_color)
         fnt = pygame.font.SysFont("8-BIT-WONDER", 85)
         Select = fnt.render("Select A Map ", 1, (255, 255, 255, 255))
         map1 = fnt.render("Sudoku 1 ", 1, (255, 255, 255, 255))
@@ -179,7 +187,6 @@ class Game(Board):
     def format_time(self, secs):
         sec = secs % 60
         minute = secs // 60
-        hour = minute // 60
         time = " " + str(minute) + ":" + str(sec)
         return time
 
@@ -333,7 +340,7 @@ class Game(Board):
                                   [1, 2, 0, 0, 0, 7, 4, 0, 0],
                                   [0, 4, 9, 2, 0, 6, 0, 0, 7]]
 
-                            self.board = Board(9, 9, 540, 540, m1)
+                            self.board = Board(9, 9, 600, 600, m1)
                             self.empty = self.board.is_empty_game()
                             self.start_state = False
                             self.playing = True
@@ -349,13 +356,13 @@ class Game(Board):
                                   [0, 2, 0, 0, 1, 0, 7, 5, 0],
                                   [3, 8, 0, 0, 7, 0, 0, 4, 2]]
 
-                            self.board = Board(9, 9, 540, 540, m2)
+                            self.board = Board(9, 9, 600, 600, m2)
                             self.empty = self.board.is_empty_game()
                             self.start_state = False
                             self.playing = True
                             # Random Sudoku
                         elif self.cursor_state == 'r' and self.transition == False:
-                            self.board = Board(9, 9, 540, 540, generate_random_sudoku())
+                            self.board = Board(9, 9, 600, 600, generate_random_sudoku())
                             self.empty = self.board.is_empty_game()
                             self.start_state = False
                             self.playing = True
